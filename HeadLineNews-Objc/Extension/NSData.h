23e1e0a452493d04da990affba8fc4c2c9465f9d
p-x9 <chi.iosdev@gmail.com>
2021-11-29 02:25:25 +0900

    Implemented PiP (#3)

---
+++ HeadLineNews-Objc/Extension/NSData.h
@@ -0,0 +1,14 @@
+//
+//  NSData.h
+//  HeadLineNews-Objc
+//
+//  Created by p-x9 on 2021/11/28.
+//  
+//
+
+#import <Foundation/Foundation.h>
+#import <AVKit/AVKit.h>
+
+@interface NSData(HeadLineNews)
+-(CMBlockBufferRef)cMBlockBuffer;
+@end
